bf656dc6d3b11ec6caf95b40d47acf22b1c4cadb
Brian Mont <briansmont830@gmail.com>
2018-04-09 22:40:45 +0000

    images included for landing, push to heroku

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,8 +4,13 @@ var Home = React.createClass({
   render: function() {
     return (
       <div>
-        <h4>Home Component</h4>
-      
+        <h4 className="center-text">LD Energy - since 1996</h4>
+        <div className="row top-pad">
+          <div className="small-2 large-4 columns"><img src="/images/welcometoLDE.jpg"></img></div>
+          <div className="small-4 large-4 columns"><img src="/images/njcust.jpg"></img></div>
+          <div className="small-6 large-4 columns"><img src="/images/choices.jpg"></img></div>
+        
+        </div>
       </div>
     );
   }
