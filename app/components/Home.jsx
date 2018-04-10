a257814231a738d4b1f6936d169a41fe7494dd00
Brian Mont <briansmont830@gmail.com>
2018-04-10 13:25:15 +0000

    wired up AWS to source images

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,9 +6,9 @@ var Home = React.createClass({
       <div>
         <h4 className="center-text">LD Energy - since 1996</h4>
         <div className="row top-pad">
-          <div className="small-2 large-4 columns"><img src="/images/welcometoLDE.jpg"></img></div>
-          <div className="small-4 large-4 columns"><img src="/images/njcust.jpg"></img></div>
-          <div className="small-6 large-4 columns"><img src="/images/choices.jpg"></img></div>
+          <div className="small-2 large-4 columns"><img src="https://s3.us-east-2.amazonaws.com/ldwebsite/welcome.jpg" alt="welcome"></img></div>
+          <div className="small-4 large-4 columns"><img src="https://s3.us-east-2.amazonaws.com/ldwebsite/njcust.jpg" alt="njcustomers"></img></div>
+          <div className="small-6 large-4 columns"><img src="https://s3.us-east-2.amazonaws.com/ldwebsite/choices.jpg" alt="choices"></img></div>
         
         </div>
       </div>
