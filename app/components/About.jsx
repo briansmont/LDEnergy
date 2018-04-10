ae411101e05b73a72b3d0209d071f8880d6ae746
Brian Mont <briansmont830@gmail.com>
2018-04-10 14:18:37 +0000

    language to ABOUT, some in choices

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,19 @@ var About = React.createClass({
   render: function() {
     return (
       <div>
-        <h4>About will go here</h4>
+        <h3>About LD Energy</h3>
+        
+        <p>Founded in 1996, LD Energy began consulting in the deregulated New Jersey natural gas market.  Over the past 20 plus years, we have developed an expertise in electric, and electricity procurement has become our primary consulting product.</p>
+        
+        <p>There are many different markets, each with there own set of utilities, rules, regulations and unique opportunities.  Our primary focuses exist within two Independent Service Organizations(ISOs):</p>
+        <ul>
+          <li><u><b>PJM:</b></u> Pennsylvania, New Jersey, Maryland</li>
+          <li><u><b>NYISO:</b></u> New York</li>
+        </ul>
+        
+        <p>LD Energy brings decades of expertise to each of our clients.  There are always new players in the industry, but few can bring the experience, market insight, and service that LD Energy provides.</p>
+        
+        <p>We build customized procurement strategies alongside of our clients to capatalize on market opportunities. A successful strategy will bridge your goals and these opportunities.</p>
       </div>
     );
   }
--- app/components/Choices.jsx
+++ app/components/Choices.jsx
@@ -4,7 +4,10 @@ var Choices = React.createClass({
   render: function() {
     return (
       <div>
-        <h4>Your choices will go here</h4>
+        <h3>Your Choices</h3>
+        <p>In a deregulated energy market, you can purchase your electricity or natural gas through a third-party supplier rather than default service through your utility.</p>
+      
+        <p>WIREUP NEW COMPONENT INCLUDING A NAVBAR VERTICAL, include basic, process, product options. On navbar click, display processes</p>
       </div>
     );
   }
