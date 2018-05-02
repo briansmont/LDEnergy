7bd780db09d5d8fa854e6869eca7e66eda35b7db
Brian Mont <briansmont830@gmail.com>
2018-05-02 19:17:08 +0000

    horizontal nav on new list, bullet points activated

--- app/components/Choices.jsx
+++ app/components/Choices.jsx
@@ -1,17 +1,23 @@
 var React = require('react');
+var fixed = '<ul><li>Deregulated markets allow you to choose your supplier for electricity & natural gas</li><li>Your utility company will continue to maintain the delivery and distribution of the commodities</li></ul>';
+var process =  '<ul><li>Consultation with the client to determine short & long term goals</li><li>LD Energy will present alternatives taking into account these goals, combined with market opportunity and risk tolerance</li></ul>';
+var products = '<ul><li>What are the goals for energy spend?</li><li>How much risk is the client comfortable with?</li><li>Take into account the time of year and the terms of various opportunities</li></ul>';
 
 var Choices = React.createClass({
   
-  makeFixed: function() {
-    document.getElementById('details').innerHTML = '<h1>boobies</h1>';
+  makeBasics: function() {
+    document.getElementById('details').innerHTML = fixed;
+    document.getElementById('link').innerHTML = '<a href="javascript:">Learn More</a>';
   },
   
-  makeIndex: function() {
-    
+  makeProcess: function() {
+    document.getElementById('details').innerHTML = process;
+    document.getElementById('link').innerHTML = '<a href="javascript:">Learn More</a>';
   },
   
-  makeCombo: function() {
-    
+  makeProduct: function() {
+    document.getElementById('details').innerHTML = products;
+    document.getElementById('link').innerHTML = '<a href="javascript:">Learn More</a>';
   },
   
   
@@ -21,16 +27,14 @@ var Choices = React.createClass({
         <h1 className="text-center page-title">Your Choices</h1>
         <p>In a deregulated energy market, you can purchase your electricity or natural gas through a third-party supplier rather than default service through your utility.</p>
       
-        <p>WIREUP NEW COMPONENT INCLUDING A NAVBAR VERTICAL, include basic, process, product options. On navbar click, display processes</p>
-      
-        <ul>
-          <li><a href="javascript:'" onClick={this.makeFixed}>Fixed</a></li>
-          <li><a href="javascript:'">Index</a></li>
-          <li><a href="javascript:'">Combination</a></li>
+        <ul className="menu">
+          <li><a href="javascript:'" onClick={this.makeBasics}>Basics</a></li>
+          <li><a href="javascript:'" onClick={this.makeProcess}>Process</a></li>
+          <li><a href="javascript:'"onClick={this.makeProduct}>Product Options</a></li>
         </ul>
         <div>
-          <p id="details">Fixed rates offer you 100% budget certainty in a market that is always changing.</p>
-        
+          <p id="details"></p>
+          <p id="link"></p>
         </div>
         
         
