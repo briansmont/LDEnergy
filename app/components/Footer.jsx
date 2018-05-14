32d44d3a0657543ca2d7fb361883aa3a94f47c74
Brian Mont <briansmont830@gmail.com>
2018-05-14 18:39:58 +0000

    links added to footer second column

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,9 +5,82 @@ var Footer = React.createClass({
     return (
       <div id="footer">
         <div className="top-bar">
-          <h4>Footer</h4>
-          <p>Footer information will live in here</p>
-        
+          
+          <div className="row">
+            <div className="medium-9 columns">
+              <div className="row">
+                <div className="medium-4 columns">
+                  <h5>Quick Links</h5>
+                </div>
+                <div className="medium-4 columns">
+                  <h5>Helpful Sources</h5>
+                </div>
+                <div className="medium-4 columns">
+                  <h5>Contact</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="medium-9 columns">
+              <div className="row">
+                <div className="medium-4 columns">
+                  <p><a href="#/">Home</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p><a href="https://www.ferc.gov" target="_blank">Federal Energy Regulatory Commission</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p>content</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="medium-9 columns">
+              <div className="row">
+                <div className="medium-4 columns">
+                  <p><a href="#/About">About Us</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p><a href="www.bpu.state.nj.us" target="_blank">NJ Board of Public Utilities</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p>content</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="medium-9 columns">
+              <div className="row">
+                <div className="medium-4 columns">
+                  <p><a href="#/Market">Market Info</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p><a href="http://www.cmegroup.com/trading/energy/natural-gas/natural-gas.html" target="_blank">Henry Hub Natural Gas</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p>content</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="medium-9 columns">
+              <div className="row">
+                <div className="medium-4 columns">
+                  <p><a href="#/Quote">Request a Quote</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p><a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></p>
+                </div>
+                <div className="medium-4 columns">
+                  <p>content</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         
       </div>
