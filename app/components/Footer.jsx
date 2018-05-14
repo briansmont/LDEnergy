8a293e86efe3b46c32d79a97a641a58cc03b7655
Brian Mont <briansmont830@gmail.com>
2018-05-14 18:53:00 +0000

    copyright & contact added

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -28,10 +28,10 @@ var Footer = React.createClass({
                   <p><a href="#/">Home</a></p>
                 </div>
                 <div className="medium-4 columns">
-                  <p><a href="https://www.ferc.gov" target="_blank">Federal Energy Regulatory Commission</a></p>
+                  <p><a href="https://www.ferc.gov" target="_blank">FERC</a></p>
                 </div>
                 <div className="medium-4 columns">
-                  <p>content</p>
+                  <p>11 W. Passaic Street</p>
                 </div>
               </div>
             </div>
@@ -46,7 +46,7 @@ var Footer = React.createClass({
                   <p><a href="www.bpu.state.nj.us" target="_blank">NJ Board of Public Utilities</a></p>
                 </div>
                 <div className="medium-4 columns">
-                  <p>content</p>
+                  <p>Rochelle Park, NJ 07662</p>
                 </div>
               </div>
             </div>
@@ -61,7 +61,7 @@ var Footer = React.createClass({
                   <p><a href="http://www.cmegroup.com/trading/energy/natural-gas/natural-gas.html" target="_blank">Henry Hub Natural Gas</a></p>
                 </div>
                 <div className="medium-4 columns">
-                  <p>content</p>
+                  <p>(201) 291-1901</p>
                 </div>
               </div>
             </div>
@@ -76,11 +76,21 @@ var Footer = React.createClass({
                   <p><a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></p>
                 </div>
                 <div className="medium-4 columns">
-                  <p>content</p>
+                  <p>info@ldenergyconsulting.com</p>
                 </div>
               </div>
             </div>
           </div>
+          
+          <div id="disclaimer">
+            <div className="row">
+              <small>LD Energy is an independent consultant. Any context, opinion, prices, or general information is for informational purposes only, and may contain inaccuracies. LD Energy gives no assurance or warranty regarding the accuracy, timeliness, or applicability of any of the contents expressed. LD Energy hereby excludes liability for any claims, losses, demands, or damages of any kind whatsoever with regard to any information, content, or services provided at our web site, including but not limited to direct, indirect, incidental, or consequential loss or damages, compensatory damages, loss of profits, or data, or otherwise.</small>
+            </div>
+            <div className="row">
+              <p>&copy Copyright 2018, LD Energy, All Rights Reserved</p>
+            </div>
+          </div>
+          
         </div>
         
       </div>
