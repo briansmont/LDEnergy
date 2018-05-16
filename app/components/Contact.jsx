b59b959ce5db1f4839965802190fea62f3669416
Brian Mont <briansmont830@gmail.com>
2018-05-16 14:30:50 +0000

    contact blurb

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -7,7 +7,7 @@ var Contact = React.createClass({
       <div className="childwidth">
         <h1 className="text-center page-title">Contact Us</h1>
         <div className="small-centered small-11 medium-6 large-5">  
-          <h3>How can we help?</h3>
+          <h6>Fill out the form below and be detailed in your request. If you have any questions, list them below and we'll respond as soon as possible. If you are interested in reviewing product options, <a href="#/Quote">Request a Quote Here</a></h6>
           <form action="https://getsimpleform.com/messages?form_api_token=6bb9c297882fcb5211b5d5a9d5e976ef" method="post">
             <input type='hidden' name='redirect_to' value='http://ldbeta.herokuapp.com/#/Thanks'/>
             <input autoFocus type="text" name="fullName" placeholder="Enter Full Name"/>
--- app/components/Quote.jsx
+++ app/components/Quote.jsx
@@ -5,7 +5,7 @@ var Quote = React.createClass({
     return (
       <div className="childwidth">
         <h1 className="text-center page-title">Request a Quote</h1>
-        <p>We look forward to working with you.  In order to provide quotes, we need the authorization form below to be completed and signed.</p>
+        <p>We look forward to working with you!</p>
           <ul><b>In order to provide quotes, we  need the following:</b>
             <li>Please complete the letter of authorization below</li>
             <li>Submit a recent utility invoice for each electricity and natural gas invoice you would like us to evaluate</li>
@@ -27,7 +27,7 @@ var Quote = React.createClass({
             </div>
           </div>
         </div><br/>
-        <h6>We'll reach out to you soon to schedule a more in depth conversation. Thanks for reaching out</h6>
+        <h6>We'll reach out to you soon to schedule a more in depth conversation. Thanks for reaching out!</h6>
       </div>
     );
   }
