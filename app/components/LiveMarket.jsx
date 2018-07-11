767f4c497c8b93b1c866f948fa11fa9ba7ec6f71
Brian Mont <briansmont830@gmail.com>
2018-07-11 13:10:23 +0000

    updated style for jsx needs

--- app/components/LiveMarket.jsx
+++ app/components/LiveMarket.jsx
@@ -8,16 +8,9 @@ var LiveMarket = React.createClass({
         <p>LIVE Market child component will be here</p>
         
         <p>State Electric Price Averages</p>
-        <iframe id="eia_widget" style="float:right;width:80%;height:400px" src="//www.eia.gov/opendata/embed/iframe.php?geoset_id=ELEC.PRICE.ALL.M&map=us_merc_en&relation_id=ELEC.PRICE.ALL.M|ENDSEC&regions=USA&relation_mode=bar&regions=USA&start=200101&end=201806"></iframe>
+        <iframe id="eia_widget" style={{float: 'right', width: '80%', height: '400px'}} src="//www.eia.gov/opendata/embed/iframe.php?geoset_id=ELEC.PRICE.ALL.M&map=us_merc_en&relation_id=ELEC.PRICE.ALL.M|ENDSEC&regions=USA&relation_mode=bar&regions=USA&start=200101&end=201806"></iframe>
         
-        <p>Natural Gas Historical Curves</p>
-        <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.RNGWHHD.D" load="iframe_load"></iframe>
-      
-        <p>Existing Gas Wells</p>
-        <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.NA1170_NUS_8.A" load="iframe_load"></iframe>
-      
-        <p>US Natural Gas Consumption</p>
-        <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.N9140US2.M" load="iframe_load"></iframe>
+
 
       </div>
     );
@@ -26,3 +19,12 @@ var LiveMarket = React.createClass({
 });
 
 module.exports = LiveMarket;
+
+        // <p>Natural Gas Historical Curves</p>
+        // <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.RNGWHHD.D" load="iframe_load"></iframe>
+      
+        // <p>Existing Gas Wells</p>
+        // <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.NA1170_NUS_8.A" load="iframe_load"></iframe>
+      
+        // <p>US Natural Gas Consumption</p>
+        // <iframe id="eia_widget" style="width:75%;height:375px" src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.N9140US2.M" load="iframe_load"></iframe>
