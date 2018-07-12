a7f4bf8bee4f4f4057b83a1d983db56aaca43d0e
Brian Mont <briansmont830@gmail.com>
2018-07-12 18:46:23 +0000

    change header

--- app/components/LiveMarket.jsx
+++ app/components/LiveMarket.jsx
@@ -5,7 +5,7 @@ var LiveMarket = React.createClass({
   render: function() {
     return (
       <div className="childwidth">
-        <h3 className="text-center page-title">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></h3>
+        <h6 className="text-center page-title">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></h6>
         
         <div className="center">
           <iframe id="eia_widget" style={{ width: '80%', height: '400px'}} src="//www.eia.gov/opendata/embed/iframe.php?geoset_id=ELEC.PRICE.ALL.M&map=us_merc_en&relation_id=ELEC.PRICE.ALL.M|ENDSEC&regions=USA&relation_mode=bar&regions=USA&start=200101&end=201806"></iframe>
