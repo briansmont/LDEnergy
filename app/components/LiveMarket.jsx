979d08331425264dd161476921c86bd253bdd982
Brian Mont <briansmont830@gmail.com>
2018-07-13 17:10:59 +0000

    style and new links

--- app/components/LiveMarket.jsx
+++ app/components/LiveMarket.jsx
@@ -5,15 +5,15 @@ var LiveMarket = React.createClass({
   render: function() {
     return (
       <div className="childwidth">
-        <p className="text-center page-title">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></p>
+        <h3 className="text-center">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></h3>
         
-        <h6>Electricity</h6>
+        <h4 className="text-center">Electricity</h4>
         <div className="center">
           <iframe id="eia_widget" style={{ width: '80%', height: '400px'}} src="//www.eia.gov/opendata/embed/iframe.php?geoset_id=ELEC.PRICE.ALL.M&map=us_merc_en&relation_id=ELEC.PRICE.ALL.M|ENDSEC&regions=USA&relation_mode=bar&regions=USA&start=200101&end=201806"></iframe>
         </div>
 
 
-        <h6>Natural Gas</h6>
+        <h4>Natural Gas</h4>
         
         <iframe id="eia_widget" style={{width:'75%',height:'375px'}} src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.RNGWHHD.D" load="iframe_load"></iframe>
       
