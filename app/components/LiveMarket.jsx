c630d555614875a19b30c5c7cfa0ec598f33575c
Brian Mont <briansmont830@gmail.com>
2018-07-13 16:41:48 +0000

    change heads market update page

--- app/components/LiveMarket.jsx
+++ app/components/LiveMarket.jsx
@@ -5,20 +5,20 @@ var LiveMarket = React.createClass({
   render: function() {
     return (
       <div className="childwidth">
-        <h6 className="text-center page-title">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></h6>
+        <p className="text-center page-title">Data below is current and sourced from the <a href="https://www.eia.gov/" target="_blank">U.S. Energy Info Administration</a></p>
         
+        <h6>Electricity</h6>
         <div className="center">
           <iframe id="eia_widget" style={{ width: '80%', height: '400px'}} src="//www.eia.gov/opendata/embed/iframe.php?geoset_id=ELEC.PRICE.ALL.M&map=us_merc_en&relation_id=ELEC.PRICE.ALL.M|ENDSEC&regions=USA&relation_mode=bar&regions=USA&start=200101&end=201806"></iframe>
         </div>
 
 
-        <p>Natural Gas Historical Curves</p>
+        <h6>Natural Gas</h6>
+        
         <iframe id="eia_widget" style={{width:'75%',height:'375px'}} src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.RNGWHHD.D" load="iframe_load"></iframe>
       
-        <p>Existing Gas Wells</p>
         <iframe id="eia_widget" style={{width:'75%',height:'375px'}} src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.NA1170_NUS_8.A" load="iframe_load"></iframe>
       
-        <p>US Natural Gas Consumption</p>
         <iframe id="eia_widget" style={{width:'75%',height:'375px'}} src="//www.eia.gov/opendata/embed/iframe.php?series_id=NG.N9140US2.M" load="iframe_load"></iframe>
 
       </div>
