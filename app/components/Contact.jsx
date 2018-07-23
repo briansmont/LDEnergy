d1aba8df02bd46b66d68b05197a76430920960db
Brian Mont <briansmont830@gmail.com>
2018-07-23 13:08:31 +0000

    links to thanks, updated gas storage reporting

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -13,7 +13,7 @@ var Contact = React.createClass({
         <div className="small-centered small-11 medium-6 large-5">  
           <h6>Fill out the form below and be detailed in your request. If you have any questions, list them below and we'll respond as soon as possible. If you are interested in reviewing product options, <a href="#/Quote">Request a Quote Here</a></h6>
           <form action="https://getsimpleform.com/messages?form_api_token=6bb9c297882fcb5211b5d5a9d5e976ef" method="post">
-            <input type='hidden' name='redirect_to' value='http://ldbeta.herokuapp.com/#/Thanks'/>
+            <input type='hidden' name='redirect_to' value='http://ldenergyconsulting.com/#/thanks'/>
             <input autoFocus type="text" name="fullName" placeholder="Enter Full Name"/>
             <input type="text" name="company" placeholder="Company Name" />
             <input type="text" name="position" placeholder="Position" />
--- app/components/Thanks.jsx
+++ app/components/Thanks.jsx
@@ -9,7 +9,18 @@ var Thanks = React.createClass({
   render: function() {
     return (
       <div className="childwidth">
-        <h3>Thanks for reaching out, we'll be in touch as soon as possible</h3>
+        <h4 className="text-center">Thanks for reaching out, we'll be in touch as soon as possible</h4><br/><br/>
+        
+        <ul><u>Check out some more information:</u>
+          <li><a href="#/Products">Available Products</a></li>
+          <li><a href="#/Services">Services We Offer</a></li>
+          <li><a href="http://www.state.nj.us/bpu/pdf/commercial/shoppingguide.pdf" target="_blank">NJ Shopping Guide</a></li>
+        </ul>
+        <ul><u>From the US Energy Information Administration:</u>
+          <li><a href="https://www.eia.gov/electricity/reports.php#/T202" target="_blank">Electricity</a></li>
+          <li><a href="https://www.eia.gov/naturalgas/reports.php#/T202" target="_blank">Natural Gas</a></li>
+        </ul>
+        
       </div>
       
     );
